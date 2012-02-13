cb60f30fe4807b784840a60071c237dfcb0ebd40
Kurt Robert Rudolph <rudolph9@illinois.edu>
2012-02-13 16:29:41 -0600

    customized to ubuntu

--- .configure/configure_git_user.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-git config --global user.name "Kurt Robert Rudolph"
-git config --global user.email rudolph9@illinois.edu
--- .login_items/ScreenAndMouseShareStarter.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-export DYLD_FORCE_FLAT_NAMESPACE=1
-export DYLD_INSERT_LIBRARIES=$HOME/Library/libfaketime/src/libfaketime.dylib.1
-export FAKETIME="@2012-01-01 01:01:01"
-
-open /Applications/ShareMouse.app
